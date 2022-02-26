5b12239d381e58d07ea23544fbf960bf905e678b
yujo <yujo.dev@gmail.com>
2022-02-26 13:51:26 +0900

    clean up

--- client/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { StrictMode } from 'react'
-import { render } from 'react-dom'
-
-render(
-  <StrictMode>
-    <h1>Hello Parcel Bundler</h1>
-  </StrictMode>,
-  document.getElementById('root')
-)
