5353258bc4347ba498fbe634140500a2a3bdacc6
yujo <yujo.dev@gmail.com>
2022-02-26 14:08:17 +0900

    chore: init

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+import Button from "antd/lib/button";
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <Button type="primary">Button</Button>;
+}
+
+export default App;
