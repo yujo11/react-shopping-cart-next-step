7aac1031fbb854698cd2d7ba1e4834ef941a0cc4
yujo <yujo.dev@gmail.com>
2022-02-27 18:33:58 +0900

    feat: Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,13 @@ import "./App.css";
 function App() {
   return (
     <Layout>
-      <Header>Header</Header>
+      <Header className="flex items-center justify-between bg-cyan-500  py-10 text-white">
+        <div className="text-4xl font-semibold">CLEAN CODE SHOP</div>
+        <div className="text-3xl ">
+          <button className="font-semibold">장바구니</button>
+          <button className="ml-4 font-semibold">주문목록</button>
+        </div>
+      </Header>
       <Content>Content</Content>
       <Footer>Footer</Footer>
     </Layout>
