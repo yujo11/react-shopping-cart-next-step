cba495ad52499283bdc39072e9fa12b7fbba7882
yujo <yujo.dev@gmail.com>
2022-02-27 18:21:40 +0900

    chore: set tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <Button type="primary">Button</Button>;
+  return (
+    <>
+      {" "}
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button type="primary">Button</Button>;
+    </>
+  );
 }
 
 export default App;
